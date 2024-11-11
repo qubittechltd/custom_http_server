05705b3c15e84711d115c0c7dba930a4e08cfdd6
Ibrahim <ceo@qubittech.co.tz>
2024-11-11 14:57:40 +0300

    no message

--- src/httpserver/extras/middleware_impl.h
+++ src/httpserver/extras/middleware_impl.h
@@ -1,7 +1,10 @@
-#pragma once
-#include <QObject>
+#ifndef __MIDDLEWARE_IMPL_H
+#define __MIDDLEWARE_IMPL_H
+
+#include <QtHttpServer/qthttpserverglobal.h>
 
 QT_BEGIN_NAMESPACE
 
 QT_END_NAMESPACE
 
+#endif //__MIDDLEWARE_IMPL_H
